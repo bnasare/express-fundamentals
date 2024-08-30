7133850b49766504a0dfd71bd89ffa83be747400
Benedict <benedictasare2@gmail.com>
2024-08-30 14:42:07 +0000

    handling requests

--- index.js
+++ index.js
@@ -2,8 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// app.use((req, res, next) => {
+//   console.log("WE GOT A NEW REQUEST");
+//   res.send("Hello Worldddd!");
+// });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/cats", (req, res) => {
+  res.send("POST CATS");
+});
+
+app.get("/cats", (req, res) => {
+  res.send("MEOWWWWWWWWWW");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF WOOF");
+});
+
+app.get("*", (req, res) => {
+  res.send("Error!");
 });
 
 app.listen(port, () => {
